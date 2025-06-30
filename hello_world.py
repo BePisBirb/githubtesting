cf25892768313e6ea84be16128e43fb22546e587
BePisBirb <scottngo007@gmail.com>
2025-06-29 17:10:51 -0700

    Added Median Function

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello There, World!")
+import math
+
+def median(input):
+  input.sort()
+  if len(input) % 2 == 0:
+    return (input[math.floor((len(input) / 2) - 1)] + input[math.floor(len(input)/2)]) / 2
+  else:
+    return input[math.floor(len(input) / 2)]
+
+print(median([0, 3, 5, 7, 10, 11, 32, 87, 100, 999]))
